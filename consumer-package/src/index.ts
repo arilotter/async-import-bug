70bffb0a05a999884129daf1454913c796ea733c
Ari Lotter <arilotter@gmail.com>
2022-11-17 14:57:01 -0500

    fixup! init

--- consumer-package/src/index.ts
+++ consumer-package/src/index.ts
@@ -1,12 +1,11 @@
-
 const x = 0 as any;
 async function main() {
   type Wasm = Awaited<typeof import("wasm-package")>;
   type WasmMatch = Wasm["WasmMatch"];
 
   const package = await import("wasm-package");
-    const match = new package.WasmMatch(x, x, x, x, x, x, x, x, x);
-    const typeAsserted: WasmMatch = match
+  const match = new package.WasmMatch(x, x, x, x, x, x, x, x, x);
+  const typeAsserted: WasmMatch = match;
 }
 
 main();
